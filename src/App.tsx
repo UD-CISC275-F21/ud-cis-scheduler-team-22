e508e1d6d6f0b6e11de3d0efc557b643071a2b36
Diou <Diou@DESKTOP-SPJ2E7L>
2021-11-17 03:24:24 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,7 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                UD CIS Scheduler
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
+        Scheduler
         </div>
     );
 }
